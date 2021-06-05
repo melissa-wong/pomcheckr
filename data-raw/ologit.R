937adc09817a45f54025be647a00931332deba69
Melissa Wong <dispagna3@gmail.com>
2021-06-05 18:33:01 -0400

    Add vignette and dataset. Closes #2

--- data-raw/ologit.R
+++ data-raw/ologit.R
@@ -2,7 +2,13 @@
 
 library(here)
 library(haven)
+library(dplyr)
+library(magrittr)
 
-ologit <- read_dta(here("data-raw", "ologit.dta"))
+ologit <- read_dta(here("data-raw", "ologit.dta")) %>%
+  mutate(apply=factor(apply, labels=c("unlikely", "somewhat likely", "very likely")),
+         pared=factor(pared, labels=c("0", "1")),
+         public=factor(public, labels=c("0", "1")))
 
 usethis::use_data(ologit, overwrite = TRUE)
+
